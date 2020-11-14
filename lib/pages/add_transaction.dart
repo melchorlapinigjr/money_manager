9aec1b37ca121fc57bd5d7bff9307db554d5dff9
Melchor Lapinig Jr <lapinigmelchorjr@gmail.com>
2020-11-14 18:39:58 +0800

    transaction view

--- lib/pages/add_transaction.dart
+++ lib/pages/add_transaction.dart
@@ -1,6 +1,7 @@
+import 'package:date_time_picker/date_time_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 import 'package:money_manager/utils.dart';
-import 'package:money_manager/widgets/header_widget.dart';
 
 class AddTransaction extends StatefulWidget {
   @override
@@ -8,21 +9,114 @@ class AddTransaction extends StatefulWidget {
 }
 
 class _AddTransactionState extends State<AddTransaction> {
+  var amountController = MaskedTextController(mask: '000.000.00');
+  String dropdownValue = "Income";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: SemiBlackbg,
       body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.all(5),
-          padding: EdgeInsets.all(5),
-          child: Column(
-            children: [
-              Text('add new transaction', style: homePageTitleStyle),
-              SizedBox(
-                height: 10,
-              ),
-            ],
+        child: Expanded(
+          child: Container(
+            margin: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    Text(
+                      'Add new transaction',
+                      style: TextStyle(color: SemiBlackbg),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  style: itemListTitleStyle,
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.title), labelText: 'Transaction Name:'),
+                ),
+                TextFormField(
+                  style: itemListTitleStyle,
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.description_sharp),
+                      hintText: 'Transaction description.',
+                      labelText: 'Description:'),
+                ),
+                DateTimePicker(
+                  icon: Icon(Icons.calendar_today_sharp),
+                  dateMask: 'd MMM, yyyy',
+                  initialValue: DateTime.now().toString(),
+                  firstDate: DateTime(2020),
+                  lastDate: DateTime(2100),
+                  dateLabelText: 'Date:',
+                  onChanged: (val) => print(val),
+                  validator: (val) {
+                    print(val);
+                    return null;
+                  },
+                  onSaved: (val) => print(val),
+                ),
+                TextFormField(
+                  style: itemListTitleStyle,
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.attach_money_sharp),
+                      hintText: 'Transaction amount.',
+                      labelText: 'Amount:'),
+                  controller: amountController,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Type: ",
+                      style: TextStyle(
+                        color: Colors.black54,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    DropdownButton<String>(
+                      value: dropdownValue,
+                      icon: Icon(Icons.arrow_downward),
+                      iconSize: 20,
+                      style: TextStyle(color: Colors.deepPurple),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.deepPurpleAccent,
+                      ),
+                      onChanged: (String newValue) {
+                        setState(() {
+                          dropdownValue = newValue;
+                        });
+                      },
+                      items: <String>['Income', 'Expense', 'Investment']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5),
+                RaisedButton.icon(
+                  icon: Icon(Icons.save_alt_rounded, size: 18),
+                  textColor: Colors.white,
+                  color: Color(0xFF6200EE),
+                  onPressed: () {
+                    // Respond to button press
+                  },
+                  label: Text("Save Transaction"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
